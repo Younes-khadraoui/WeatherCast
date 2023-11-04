3c037a7ed934b9ac24382065999d7ba3ac7bfce7
younes khadraoui <younes.khadraoui.pro@gmail.com>
2023-11-04 17:04:11 +0100

    chore : fixed the title and the icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 import Header from "../components/header/Header";
 import Hero from "../components/main/Hero";
 import Footer from "@/components/footer";
+import Head from "next/head";
 
 export default function Home() {
   const [city, setCity] = useState("");
 
   return (
     <main>
+      <Head>
+        <title>WeatherCast</title>
+      </Head>
       <div className="absolute main -z-10"></div>
       <Header city={city || "London"} />
       <Hero
